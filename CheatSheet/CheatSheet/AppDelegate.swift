cc72b338a97d9701e5049b576ee83908f7aa59bc
rajul-arora <rajularora1994@gmail.com>
2016-11-11 20:38:00 -0800

    Initial commit

---
+++ CheatSheet/CheatSheet/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CheatSheet
+//
+//  Created by Rajul Arora on 2016-11-11.
+//  Copyright © 2016 CheatSheet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
